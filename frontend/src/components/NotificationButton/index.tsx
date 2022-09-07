78905a34f39706612b449c11592192ab76a89def
josuel mroczko <105312703+josuelmroczko@users.noreply.github.com>
2022-09-07 09:49:32 -0300

    other components

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,10 +1,11 @@
-import icon from '../../assets/img/notification-icon.svg'
+import aicon from '../../assets/img/Notification-icon.svg'
+
 import './styles.css'
 
 function NotificationButton() {
     return (
         <div className="dsmeta-red-btn">
-            <img src={icon} alt="Notificar" />
+            <img src={aicon} alt="Notificar" />
         </div>
     )
 }
